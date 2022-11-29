a4f2c3d9d67e5c8749b6fb5f467dd6e329af049e
Stephanie CP <stephers.cp@gmail.com>
2022-11-28 17:49:30 -0800

    added comment to clarify filed to be deleted

--- group4PageObject.ts
+++ group4PageObject.ts
@@ -1,4 +1,6 @@
-// Need to fill in
+// WILL BE DELETED, EVERYONE ON THE TEAM SHOULD CREATE THEIR OWN [NAME]PageObject.ts
+
+
 // See Mars's example from 11/23 class: https://github.com/MarohnHoward/group-number-projectpt9/blob/main/marsPageObject.ts
 
 import {By} from 'selenium-webdriver'
