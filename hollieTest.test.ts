4e6283d88d77f2d77965680ba4f28be1fe289625
Stephanie CP <stephers.cp@gmail.com>
2022-12-02 13:38:29 -0800

    Commiting Hollie's code

---
+++ hollieTest.test.ts
@@ -0,0 +1,17 @@
+import {Hollie} from './holliePageObject'
+const hollie = new Hollie()
+
+describe('testing Hollie stuff', () => {
+    test('can click back a bunch', async () => {
+        await hollie.navigate()
+        await hollie.click(hollie.bookBtn)
+        await hollie.repeatClick(10, hollie.backBtn)
+        await hollie.click(hollie.todayBtn)
+        let currentDate = await hollie.getText(hollie.dateBanner)
+        expect(currentDate).toContain("November 2022")
+        await hollie.driver.quit()
+
+        
+    }) 
+
+})
